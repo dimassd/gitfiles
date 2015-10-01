134afbd75bf0e7ada101b02e8478b5bfc8d8f723
dmitry <dmitry@dima>
2015-10-01 23:20:32 +0300

    Add

---
+++ osgi/src/osgi/Activator.java
@@ -0,0 +1,25 @@
+package osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("Hello World!!!");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Goodbye World!!!");
+	}
+
+}
